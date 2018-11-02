f6feef3ee053f39728e7652ee1622de91b8b4dbf
Paul Mumby <paul@orb0.com>
2018-11-02 13:54:26 +1300

    Heavy refactor of data model schema Fixed a handful of bugs Refactors to support new model schema A few other minor tweaks here and there for demo app

--- src/windlass-core.js
+++ src/windlass-core.js
@@ -37,18 +37,19 @@ class Windlass {
 
     initModels() {
         this.logSilent("Initializing Object Models...")
-        this.logSilent(this.config.models.length + " Models Found...")
-        for (const modelData of this.config.models) {
-            this.logSilent("...Initializing Model: " + modelData.name)
-            this.dataCache[modelData.name] = new Map()
+        this.logSilent(Object.keys(this.config.datamodel).length + " Models Found...")
+        for (const modelName in this.config.datamodel) {
+            let modelData = this.config.datamodel[modelName]
+            this.logSilent("...Initializing Model: " + modelName)
+            this.dataCache[modelName] = new Map()
             let newModel = Object.assign(
-                this.buildObjectMethods(modelData),
+                this.generateObjectMethods(modelName,modelData),
                 {config:modelData}
                 //TODO: Possibly other objects to compose for model
             )
-            this.models[modelData.name] = newModel
-            this.setupNewObjectHooks(modelData)
-            this.enumerateObjects(modelData.name)
+            this.models[modelName] = newModel
+            this.setupNewObjectHooks(modelName)
+            this.enumerateObjects(modelName)
         };
     }
 
@@ -61,31 +62,38 @@ class Windlass {
 
     async fetchObjectData(modelName, indexValue) {
         let modelData = this.models[modelName].config
-        let modelContract = modelData.contract
+        let modelContract = modelData.primaryContract
         let core = this.chain.contractInstances[modelContract]
         let obj = new Map()
-        for (const attribute of modelData.attributes) {
-            if (attribute.type == "index") {
-                obj.set(attribute.name, indexValue)
+        for (const propertyName in modelData.properties) {
+            let property = modelData.properties[propertyName]
+            if (propertyName === modelData.primaryKey) {
+                obj.set(propertyName, indexValue)
             } else {
-                let attribRawValue = await core[attribute.getter].call(indexValue)
-                if (attribute.type == "number") {
-                    obj.set(attribute.name, +(attribRawValue.valueOf()))
+                let attribRawValue = await core[property.getter].call(indexValue)
+                if (property.type === "integer") {
+                    obj.set(propertyName, +(attribRawValue.valueOf()))
                 } else {
-                    obj.set(attribute.name, attribRawValue)
+                    obj.set(propertyName, attribRawValue)
                 }
-                //TODO: Likely need to handle other cases here...
+                //TODO: Definitely need to flesh this out to handle other types, etc...
             }
         }
         return obj
     }
 
     watchObject(modelName, indexValue) {
+        //TODO: Do this with ws API if possible, for true push (reduce load on JS for constantly polling on new block)
         let modelData = this.models[modelName].config
         let hooks = [];
-        for (const event of modelData.events) {
-            if (event.indicatesUpdate) {
-                hooks.push(event.contract + "." + event.name + ":" + event.indexField)
+        for (const contractName in modelData.events) {
+            let contract = modelData.events[contractName]
+            for (const eventName in contract) {
+                let event = contract[eventName]
+                if (event.indicatesUpdate) {
+                    //TODO: Refactor this to not be a string, make it an object
+                    hooks.push(contractName + "." + eventName + ":" + event.indexField)
+                }
             }
         }
         if (hooks.length > 0) {
@@ -96,29 +104,45 @@ class Windlass {
     }
 
     async refreshObject(modelName, indexValue) {
-        let modelData = this.models[modelName].config
-        this.dataCache[modelData.name].set(indexValue, await this.fetchObjectData(modelName, indexValue))
+        this.dataCache[modelName].set(indexValue, await this.fetchObjectData(modelName, indexValue))
         this.dataUpdated()
     }
 
-    async enumerateObjects(modelName) {
+    async countObjects(modelName) {
+        //TODO: Handle additional enumerators
         let modelData = this.models[modelName].config
-        this.logContract(modelData.name,"Enumerating Objects...","DEBUG",0)
-        let modelContract = modelData.contract
+        let modelContract = modelData.primaryContract
+        let defaultEnumerator = modelData.enumerators[modelData.defaultEnumerator]
         let core = this.chain.contractInstances[modelContract]
-        let objCount = await core[modelData.enumerator.method].call.apply(this, modelData.enumerator.extraParams)
-        for (let i = 0; i < objCount; i++) {
-            this.watchObject(modelName, i);
-        }
-        this.logContract(modelData.name,"Enumeration Complete!","DEBUG",0)
+        let objCount = await core[defaultEnumerator.method].call.apply(this, defaultEnumerator.parameters)
+        return objCount
     }
 
-    setupNewObjectHooks(modelData) {
-        let modelName = modelData.name
+    async enumerateObjects(modelName, limit=0, offset=0, filters=[]) {
+        //TODO: Additional Enumerators & Handle Filters
+        let modelData = this.models[modelName].config
+        this.logContract(modelName,"Enumerating Objects...","DEBUG",0)
+        let modelContract = modelData.primaryContract
+        let defaultEnumerator = modelData.enumerators[modelData.defaultEnumerator]
+        let core = this.chain.contractInstances[modelContract]
+        let objCount = await core[defaultEnumerator.method].call.apply(this, defaultEnumerator.parameters)
+        for (let i = offset; i < offset+limit && i < objCount; i++) {
+            this.watchObject(modelName, i);
+        }
+        this.logContract(modelName,"Enumeration Complete!","DEBUG",0)
+    }
+
+    setupNewObjectHooks(modelName) {
+        let modelData = this.models[modelName].config
         let hooks = []
-        for (const event of modelData.events) {
-            if (event.indicatesCreation) {
-                hooks.push(event.contract + "." + event.name + ":" + event.indexField)
+        for (const contractName in modelData.events) {
+            let contract = modelData.events[contractName]
+            for (const eventName in contract) {
+                let event = contract[eventName]
+                if (event.indicatesCreation) {
+                    //TODO: Refactor this to not be a string, make it an object
+                    hooks.push(contractName + "." + eventName + ":" + event.indexField)
+                }
             }
         }
         if (hooks.length > 0) {
@@ -127,54 +151,57 @@ class Windlass {
         }
     }
 
-    buildObjectMethods(modelData) {
-        let modelContract = modelData.contract
+    generateObjectMethods(modelName,modelData) {
+        let modelContract = modelData.primaryContract
         let core = this.chain.contractInstances[modelContract]
         let methods = {}
         //Generate Setter Function Wrappers
-        for (const attribute of modelData.attributes) {
-            if (attribute.hasOwnProperty("setter")) {
+        for (const propertyName in modelData.properties) {
+            let property = modelData.properties[propertyName]
+            if (property.hasOwnProperty("setter")) {
                 let fSetter = async function(index, value) {
-                    let funcString = "Update to " + modelData.name + " ID:" + index + " " + attribute.friendlyName + " To Value: [" + value + "]"
+                    let funcString = "Update to " + modelName + " ID:" + index + " " + property.title + " To Value: [" + value + "]"
                     let logStringReq = "Requesting " + funcString + "..."
                     let logStringSuccess = funcString + " was Successful!"
                     this.logContract(modelContract, logStringReq)
                     let txCallback = (txData, txLogs) => {
                         this.logContract(modelContract, logStringSuccess)
                     }
-                    let txResult = await core[attribute.setter](index, value, { from: this.chain.getAccount() })
+                    let txResult = await core[property.setter](index, value, { from: this.chain.getAccount() })
                     this.chain.watchTX(txResult, txCallback)
                     return txResult
                 }
-                methods[attribute.setter] = fSetter.bind(this)
+                methods[property.setter] = fSetter.bind(this)
             }
         }
         //Generate Action Function Wrappers
-        for (const action of modelData.actions) {
+        for (const actionName in modelData.actions) {
+            let action = modelData.actions[actionName]
             let fAction = async function(index) {
                 //TODO: Handle extra params!
-                let funcString = action.friendlyName+" on "+modelData.name+" ID:"+index
+                let funcString = action.title+" on "+modelName+" ID:"+index
                 let logStringReq = "Requesting " + funcString + "..."
                 let logStringSuccess = funcString + " was Successful!"
                 this.logContract(modelContract, logStringReq)
                 let txCallback = (txData, txLogs) => {
                     this.logContract(modelContract, logStringSuccess)
                 }
-                let txResult = await core[attribute.setter](index, value, { from: this.chain.getAccount() })
+                let txResult = await core[action.method](index, value, { from: this.chain.getAccount() })
                 this.chain.watchTX(txResult, txCallback)
                 return txResult
             }
-            methods[action.name] = fAction.bind(this)
+            methods[actionName] = fAction.bind(this)
         }
         //Generate Constructor Function Wrapper
         let fConstructor = async function (...args) {
-            let funcString = modelData.constructor.friendlyName
+            let funcString = modelData.constructor.title
             let logStringReq = "Requesting " + funcString + "..."
             let logStringSuccess = funcString + " was Successful!"
             this.logContract(modelContract, logStringReq)
             let txCallback = (txData, txLogs) => {
                 this.logContract(modelContract, logStringSuccess)
             }
+            //TODO: Review, and more robust handling of this. Validation, etc. Based on model.
             let paramArray = args.concat([{ from: this.chain.getAccount() }])
             let txResult = await core[modelData.constructor.method].apply(this,paramArray)
             this.chain.watchTX(txResult, txCallback)
